0ecbb0ccb4f235713fdbda75c15979bd20fbeca8
Maha Mahmood <>
2020-11-11 20:22:37 -0500

    second solution to chunked array

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,7 +8,7 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-
+// solution 1
 // Pesudocode
 // Create empty array to hold chunks called 'chunked'
 // For each element in the 'unchunked' array
@@ -17,21 +17,39 @@
 // // // Push a new chunk into 'chunked' with the current element
 // // Else add the current element into the chunk
 
-function chunk(array, size) {
-    const chunked = [];
+// function chunk(array, size) {
+//     const chunked = [];
 
-    for (let element of array) {
-        const last = chunked[chunked.length - 1];
-        if (!last || last.length === size) {
-            chunked.push([element]);//pushing array & element at the same time
-        } else {
-            last.push(element);
-        }
+//     for (let element of array) {
+//         const last = chunked[chunked.length - 1];
+//         if (!last || last.length === size) {
+//             chunked.push([element]);//pushing array & element at the same time
+//         } else {
+//             last.push(element);
+//         }
+//     }
+//     console.log(chunked);
+//     return chunked;
+// }
+
+// solution 2
+// Pesudocode
+// use method .slice(0, 3) 3 won't be included
+// create empty 'chunked' array
+// create 'index' variable start at 0
+// loop while index is less than array.length
+// // push a slice of length 'size' from 'array' into 'chunked'
+
+const chunk = (array, size) => {
+    const chunked = [];
+    let index = 0;
+
+    while (index < array.length) {
+        chunked.push(array.slice(index, index + size));
+        index += size;
     }
-    console.log(chunked);
     return chunked;
 }
-
 
 chunk([1, 2, 3, 4, 5, 6, 7, 8], 3);
 
