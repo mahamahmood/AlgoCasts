e46b4b9e74723595faa72b0a807e2bc5a450f170
Maha Mahmood <>
2020-11-11 20:55:34 -0500

    solved capitalized str

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,27 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+
+// pesudocode
+// make an empty array 'words' that will hold the capitalized str
+// split the input str by spaces to get an array
+// for each word of the array
+// // uppercase the first letter of the word
+// // join the first letter with the rest of the string
+// // push the result into 'words' array
+// join 'words' into a string and return it
+
+function capitalize(str) {
+    const words = [];
+    const splitStr = str.split(' ');
+
+    for (let word of splitStr) {
+        words.push(word[0].toUpperCase() + word.slice(1));
+    }
+
+    return words.join(' ');
+    
+}
+capitalize('hello world');
 
 module.exports = capitalize;
