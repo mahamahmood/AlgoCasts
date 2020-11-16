d3e2b4521d5889c5bf576210dcef63f1f489e86b
Maha Mahmood <>
2020-11-16 17:07:06 -0500

    second solution to steps using recursion

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -42,5 +42,28 @@ const steps = (n) => {
 
 }
 
+// solution 2 Recursion
+// if (row === n) then we have hit the end of our problem
+// if the 'stair' string has a length === n then we are at the end of a row
+// if the length of the stair string is less than or equal to the row number we're working, we add a '#', otherwise add a space
+
+function steps(n, row = 0, stair = '') {
+    if (n === row) {
+        return;
+    }
+
+    if (n === stair.length) {
+        console.log(stair);
+        return steps(n, row + 1);
+    }
+
+    if (stair.length <= row) {
+        stair += '#';
+    } else {
+        stair += ' ';
+    }
+    steps(n, row, stair);// to restart the process again
+}
+
 steps(5);
 module.exports = steps;
