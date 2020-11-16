fd337b33815df0b36d18fd354fbd7c7de5a9ffc3
Maha Mahmood <>
2020-11-16 13:52:46 -0500

    solved steps challenge

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,30 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// pseudocode
+// create a for loop from 0 to n (iterate through rows)
+// // create an empty string, 'stair'
+// // from 0 to n (iterate through columns)
+// // // if the current col is equal to or less than the current row
+// // // // add a '#' to 'stair'
+// // // else
+// // // // add a space to 'stair'
+// // console log 'stair'
+const steps = (n) => {
+    for (let row = 0; row < n; row++) {
+        let stair = '';
 
+        for (let column = 0; column < n; column++) {
+            if (column <= row) {
+                stair += '#';
+            } else {
+                stair += ' ';
+            }
+        }
+        console.log(stair);
+    }
+
+}
+
+steps(5);
 module.exports = steps;
